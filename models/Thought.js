14d74827672b1ce27596d634c2c011fcc0249d74
MFG <macfglenn@gmail.com>
2022-03-09 23:03:20 -0500

    added index files, weird error

--- models/Thought.js
+++ models/Thought.js
@@ -1,25 +1,6 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema],
-});
-
 const reactionSchema = new Schema(
   {
     reactionId: {
@@ -49,6 +30,25 @@ const reactionSchema = new Schema(
     id: false,
   }
 );
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+});
 
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
